21b6f4254e5359d950cfddc5e40ad80a9dd4af77
Akram Roubia <akram.roubia@etu.univ-grenoble-alpes.fr>
2024-12-06 00:15:52 +0100

    Bad-UI V2 (PlayPause & Volume separated + Fully Randomized Volume) completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Container, CssBaseline, Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PlayPodcastButton from './components/PlayPodcastButton';
+import PlayPauseControl from './components/PlayPauseControl';
+import VolumeControl from './components/VolumeControl';
 
 const App: React.FC = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
   return (
     <Router>
       <CssBaseline />
@@ -19,9 +22,13 @@ const App: React.FC = () => {
           </Routes>
         </Box>
       </Container>
-      <PlayPodcastButton /> {/* Add this line to render the CanvasOverlay */}
+      {/* Audio element */}
+      <audio ref={audioRef} src="podcast2.mp3" preload="auto" />
+      {/* Controls */}
+      <PlayPauseControl audioRef={audioRef} />
+      <VolumeControl audioRef={audioRef} />
     </Router>
   );
-}
+};
 
 export default App;
