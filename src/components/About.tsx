c9d41856e22398d835d7c079a7383b9cdb5c07a1
Nicolas Picaud <nicolas.picaud@etu.univ-savoie.fr>
2024-12-06 06:31:56 +0100

    Update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,11 +12,11 @@ const About: React.FC = () => {
       </Typography>
       <ul>
         <li>PICAUD Nicolas : Leader + Implémentation DevOps + Implémentation Défi API Météo</li>
-        <li>ROCHDI Rana : Implémentation Parallax</li>
+        <li>ROCHDI Rana : Implémentation Parallax + Podcast</li>
         <li>MAHAMOUDOU Hannane : Design Site + Implémentation QCM + Écriture du Texte Défi Principal</li>
         <li>CHERBLANC Noah : Implémentation Défi QR Codes Pokémon</li>
-        <li>ROSANO Romain : Implémentation Défi CAPTCHA Interactif</li>
-        <li>ROUX Yann : Implémentation Défi Logo Caché</li>
+        <li>ROSANO Romain : Implémentation Défi CAPTCHA Interactif + Implémentation Défi QR Codes Pokémon + Podcast</li>
+        <li>ROUX Yann : Implémentation Défi Logo Caché + Implémentation Défi Dark UX</li>
         <li>ROUBIA Akram: Implémentation Défi CAPTCHA Interactif + Implémentation Défi Dark UX</li>
       </ul>
     </div>
